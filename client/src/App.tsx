b20ebd5b4df90b75c6930ed8621bed008a3601ea
Ben cohen <83662617+Benco351@users.noreply.github.com>
2023-03-26 00:48:54 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ProductsContextProvider from './contexts/Products';
 import Main from './components/Main';
-/* <TasksContextProvider> */
-// </TasksContextProvider>
 
 const App = () => {
   return (
